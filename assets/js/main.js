d455b8ecd98120e0b98f4e83dfd3daa2d1ada1f6
freetimezp <kraftukrnet@ukr.net>
2024-01-31 20:32:07 +0200

    04. js-portfolio-44-interno. Header section. Toggle menu.

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+const navTriggerBtn = document.getElementById('nav_trigger_btn');
+const navMenu = document.getElementById('nav_menu');
+
+navTriggerBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('nav-is-open');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
