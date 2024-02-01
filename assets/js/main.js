b5ead8468509d1730ddec3be399d7d3a657bc637
freetimezp <kraftukrnet@ukr.net>
2024-02-01 18:23:43 +0200

    07. js-portfolio-44-interno. Testimonial section.

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,27 @@ navTriggerBtn.addEventListener('click', () => {
 });
 
 
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    slidesPerView: 3,
+    spaceBetween: 20,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        960: {
+            slidesPerView: 2,
+        },
+        1200: {
+            slidesPerView: 3,
+        },
+    }
+});
+
 
 
 
